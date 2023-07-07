bbc63d42b229f2dfea302a06f66d316a62573994
alitnp <aligha367@gmail.com>
2023-07-07 09:17:19 +0330

    domain chabge

--- apps/web/src/global/constants/webConfig.ts
+++ apps/web/src/global/constants/webConfig.ts
@@ -1,6 +1,6 @@
 const webConfig = {
-	baseUrl: process.env.baseUrl,
-	domain: process.env.domain,
+	baseUrl: "http://185.116.160.30:4000/api/v1",
+	domain: "http://185.116.160.30:4000",
 	dataRevalidateTime: 2,
 	cookieTokenName: "5gv0-43g_ddd43f",
 };
